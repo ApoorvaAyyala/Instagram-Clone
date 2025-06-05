ae01d9fb22dd16997ce7be46827c2404ca5416db
Apoorva Ayyalasomayajula <73791553+ApoorvaAyyala@users.noreply.github.com>
2025-06-05 18:25:12 +0530

    Create Footer.jsx

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-center w-100 footer justify-center'>
+      <a href='#' className='ms-3 '>
+        Contact Us
+      </a>
+      <a href='#' className='ms-3 '>
+        About Us
+    </a>
+    </div>
+  )
+}
+
+export default Footer
